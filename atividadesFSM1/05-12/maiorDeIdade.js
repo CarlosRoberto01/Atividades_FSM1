45b6fa96a825e3abb87779cff2f372cbdcdccc5d
Carlos Roberto <carloos.bezerra@hotmail.com>
2024-12-16 13:12:21 -0300

    Atividades dia 05-12

--- atividadesFSM1/05-12/maiorDeIdade.js
+++ atividadesFSM1/05-12/maiorDeIdade.js
@@ -1,14 +1,11 @@
-//Verifica Se é maior de idade ou não 
-function verificaMaiorDeIdade(idade){
-    return idade >=18 ? "É maior de idade" : "É menor de idade";
-}
-
 //Solicita idade para Usuário
-const inputIdade = prompt("Digite sua idade: ");
+const inputIdade = window.prompt("Digite sua idade: ");
 
 const idade = parseInt(inputIdade);
 
-//Exibe mensagem para usuario
-const mensagem = verificaMaiorDeIdade(idade);
-    alert(mensagem);
-    console.log(mensagem);
+//Verifica Se é maior de idade ou não 
+function verificaMaiorDeIdade(idade) {
+    return idade >= 18 ? document.write("É maior de idade.") : document.write("É menor de idade.");
+}
+
+verificaMaiorDeIdade(idade);
