0bd1ec0092a494a6b81ada7ca9ad5a8ed5342edf
Carlos Roberto <carloos.bezerra@hotmail.com>
2024-12-23 18:48:38 -0300

    Gerando número aleatorio e mostrando na tela

--- atividadesFSM1/desafioRecesso/assets/js/script.js
+++ atividadesFSM1/desafioRecesso/assets/js/script.js
@@ -1,1 +1,14 @@
-console.log("Começando")
+console.log("Começando");
+
+//Gerando numero aleatório
+const menorValor = 1;
+const maiorValor = 100;
+
+function gerarNumeroAleatorio() {
+    return parseInt(Math.random() * maiorValor);
+};
+
+const elementoNumeroAleatorio = document.getElementById('numero-aleatorio');
+elementoNumeroAleatorio .innerHTML = gerarNumeroAleatorio();
+
+
