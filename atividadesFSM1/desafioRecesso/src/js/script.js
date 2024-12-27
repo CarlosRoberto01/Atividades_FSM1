bdb0e941f773452465d91be160be7a7d359057c8
Carlos Roberto <carloos.bezerra@hotmail.com>
2024-12-27 09:03:36 -0300

    Criando cronômetro com botões de iniciar, pausar e resetar

--- atividadesFSM1/desafioRecesso/src/js/script.js
+++ atividadesFSM1/desafioRecesso/src/js/script.js
@@ -1,5 +1,3 @@
-console.log("Começando");
-
 //Gerando numero aleatório
 const menorValor = 1;
 const maiorValor = 100;
@@ -15,6 +13,46 @@ elementoNumeroAleatorio.innerHTML = gerarNumeroAleatorio();
 
 const elementoCronometroMinutos = document.getElementById("cronometro-minuto");
 const elementoCronometroSegundos = document.getElementById("cronometro-segundo");
+
+let minutos = 0;
+let segundos = 0;
+let intervalo;
+
+function formatarNumero(numero) {
+    return numero.toString().padStart(2, "0");
+}
+//Função atualizar o cronometro
+function atualizarCronometro(){
+    segundos ++;
+
+    if (segundos == 60){
+        segundos = 0;
+        minutos ++;
+    }
+    elementoCronometroMinutos.textContent = `${formatarNumero(minutos)} :`;
+    elementoCronometroSegundos.textContent = formatarNumero(segundos);
+};
+//Função botao iniciar
+function iniciarCronometro(){
+    if (!intervalo) {
+        intervalo = setInterval(atualizarCronometro, 1000);
+    }
+};
+//Função botao pausar
+function pausarCronometro(){
+    clearInterval(intervalo);
+    intervalo = null;
+};
+//Função botao resetar
+function resetarCronometro(){
+    clearInterval(intervalo);
+    intervalo = null;
+    minutos = 0;
+    segundos = 0;
+    elementoCronometroMinutos.textContent = "00 :";
+    elementoCronometroSegundos.textContent = "00";
+};
+
 
 
 
