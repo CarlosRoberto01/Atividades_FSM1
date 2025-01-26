0d6a983f8fa1f3100b6a07504f0d3fef44df4f7a
Carlos Roberto <carloos.bezerra@hotmail.com>
2025-01-26 15:40:28 -0300

    Adicionando os componentes: Card e About; Estilizando os componentes

---
+++ atividadesFSM1/2025/13-01-25/vite-project/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">MinhaLogo</div>
+      <ul className="navbar-links">
+        <li><a href="#home" className="navbar-link">Home</a></li>
+        <li><a href="#sobre" className="navbar-link">Sobre</a></li>
+        <li><a href="#servicos" className="navbar-link">Serviços</a></li>
+        <li><a href="#contato" className="navbar-link">Contato</a></li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
--- atividadesFSM1/2025/13-01-25/vite-project/src/main.jsx
+++ atividadesFSM1/2025/13-01-25/vite-project/src/main.jsx
@@ -1,10 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import Footer from './components/Footer.jsx'
+import Navbar from './components/Navbar.jsx'
 import './index.css'
 import App from './App.jsx'
+import Card from './components/Card';
+import About from './components/About';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <Navbar />
     <App />
+    <Card/>
+    <About/>
+    <Footer />
   </StrictMode>,
 )
