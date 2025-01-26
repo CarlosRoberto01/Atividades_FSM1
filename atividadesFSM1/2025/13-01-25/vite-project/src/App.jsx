d86bdc568a8918a6ae1b61de1ee398d5c5181d1f
Carlos Roberto <carloos.bezerra@hotmail.com>
2025-01-26 13:59:48 -0300

    Primeira atividade com react + vite

---
+++ atividadesFSM1/2025/13-01-25/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Olá, mundo!</h1>
+      <p>Este é meu primeiro projeto com React + vite.</p>
+      <p>Carlos Roberto</p>
+    </>
+  )
+}
+
+export default App
